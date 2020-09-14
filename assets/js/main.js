85bc3adc127ede937f205ccf5bc1e1b5bd4976e7
WatxoGitHub <watxo0211@gmail.com>
2020-09-14 18:49:02 -0500

    archivo main.js correción

--- assets/js/main.js
+++ assets/js/main.js
@@ -91,10 +91,10 @@
     			$.ajax({
       				url: $(this).attr('action'),
       					beforeSend: function(){
-        				form.prepend( form_status.html('<p><i class="fa fa-spinner fa-spin"></i> Email is sending...</p>').fadeIn() );
+        				form.prepend( form_status.html('<p><i class="fa fa-spinner fa-spin"></i> Enviando correo...</p>').fadeIn() );
       				}
     			}).done(function(data){
-      				form_status.html('<p class="text-success">Thank you for contact us. As early as possible  we will contact you</p>').delay(3000).fadeOut();
+      				form_status.html('<p class="text-success">Gracias por contactarnos. Lo ante posible nos comunicaremos contigo</p>').delay(3000).fadeOut();
     			});
   			});
 
